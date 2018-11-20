6d9e7ee09863ba4827e43bac9ef1ed1410c2ed85
Roland Hartanto <rolandhartanto@gmail.com>
2018-11-20 17:05:24 +0700

    add schedule attributes, constructor, & methods

--- src/main/java/com/dusdus/scheduler/Schedule.java
+++ src/main/java/com/dusdus/scheduler/Schedule.java
@@ -1,4 +1,28 @@
 package com.dusdus.scheduler;
 
+import java.util.Calendar;
+
 public class Schedule {
+    private String start;
+    private int hour;
+    private int day;
+
+    public Schedule(String start) {
+        this.start = start;
+        String[] schedule = start.split(",");
+        hour = Integer.parseInt(schedule[0]);
+        day = Integer.parseInt(schedule[1]);
+    }
+
+    public int getHour() {
+        return hour;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public String getStartTimeString() {
+        return start;
+    }
 }
