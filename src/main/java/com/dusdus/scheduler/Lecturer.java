9c34e8937270c2a6bcf1082f6bbb298f5daa7af8
reeechart <ferdinandusrichard@yahoo.co.id>
2018-11-20 17:16:48 +0700

    add lecturer details

--- src/main/java/com/dusdus/scheduler/Lecturer.java
+++ src/main/java/com/dusdus/scheduler/Lecturer.java
@@ -1,4 +1,21 @@
 package com.dusdus.scheduler;
 
+import java.util.ArrayList;
+
 public class Lecturer {
+    private String name;
+    private ArrayList<Schedule> availability;
+
+    public Lecturer(String name) {
+        this.name = name;
+        this.availability = new ArrayList<Schedule>();
+    }
+
+    public void addSchedule(Schedule schedule) {
+        availability.add(schedule);
+    }
+
+    public String getName() {
+        return name;
+    }
 }
