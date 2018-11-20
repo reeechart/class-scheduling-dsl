7d97bed50fec498ad07880b465852536adae0836
Vincent Endrahadi <vincentendrahadi01@gmail.com>
2018-11-20 17:00:27 +0700

    Added getter and setter for classroom class

--- src/main/java/com/dusdus/scheduler/Classroom.java
+++ src/main/java/com/dusdus/scheduler/Classroom.java
@@ -1,9 +1,50 @@
 package com.dusdus.scheduler;
 
+import java.util.ArrayList;
+
 public class Classroom {
     private String id;
-    private String[] facilities;
-    private int capacity;
+    private ArrayList<String> facilities;
+    private Integer capacity;
 
-    public Classroom() {}
+    public Classroom(String id, Integer capacity) {
+        facilities = new ArrayList<>();
+
+        this.id  = id;
+        this.capacity = capacity;
+    }
+    public Classroom(String id, Integer capacity, ArrayList<String> facilities) {
+        this.id  = id;
+        this.capacity = capacity;
+        this.facilities = facilities;
+    }
+
+
+    public Integer getCapacity() {
+        return capacity;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public ArrayList<String> getFacilities() {
+        return facilities;
+    }
+
+    public void setFacilities(ArrayList<String> facilities) {
+        this.facilities = facilities;
+    }
+
+    public void setCapacity(Integer capacity) {
+        this.capacity = capacity;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void addFacility(String facility) {
+        this.facilities.add(facility);
+    }
 }
